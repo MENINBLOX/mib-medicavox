e27dec5a198efe09df64c5b407ecd1f09866a1ee
helloheesu <heesu.jung.developer@gmail.com>
2025-08-18 18:20:52 +0900

    use component instead of module css
    - LanguageCard
    - Center

--- src/app/language/page.tsx
+++ src/app/language/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { Card, Flex, Typography } from 'antd';
+import { Flex, Typography } from 'antd';
 import {
   AppLanguage,
   LANGUAGE_OPTIONS,
   useLanguageStore,
 } from '@/stores/languageStore';
-import styles from '../../page.module.css';
-import langStyles from './page.module.css';
+import LanguageCard from '@/components/language/LanguageCard';
+import Center from '@/components/common/Center';
 import { useRouter } from 'next/navigation';
 import { useAuthStore } from '@/stores/authStore';
 
-const { Title, Text } = Typography;
+const { Title } = Typography;
 
 export default function LanguageSelectionPage() {
   const { language, setLanguage } = useLanguageStore();
@@ -27,25 +27,20 @@ export default function LanguageSelectionPage() {
   };
 
   return (
-    <div className={styles.centerLayout}>
+    <Center>
       <Flex vertical align="center" gap={16}>
         <Title level={2}>Select Language</Title>
         <Flex gap={16} justify="center" wrap>
           {LANGUAGE_OPTIONS.map((opt) => (
-            <Card
+            <LanguageCard
               key={opt.code}
-              hoverable
+              language={opt}
+              selected={language === opt.code}
               onClick={() => handleClickLanguage(opt.code)}
-              className={`${langStyles.card} ${
-                language === opt.code ? langStyles.selected : ''
-              }`}
-            >
-              <div className={langStyles.flag}>{opt.flag}</div>
-              <Text strong>{opt.nativeLabel}</Text>
-            </Card>
+            />
           ))}
         </Flex>
       </Flex>
-    </div>
+    </Center>
   );
 }
---
+++ src/components/common/Center.tsx
@@ -0,0 +1,15 @@
+export default function Center({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      {children}
+    </div>
+  );
+}
